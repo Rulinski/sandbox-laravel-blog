484422cd9b1b7fa26cf2559a7b670958c0884373
vitali.rulinski <vitali.rulinski@altoros.com>
2025-05-01 21:29:02 +0200

    feat: Add post creation functionality with UI and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./Components/Navigation"
 import {Route, Routes} from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Create from "./Pages/Posts/Create";
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
+                <Route path="/posts/create" element={<Create/>}/>
             </Routes>
         </div>
     )
--- frontend/src/Pages/Posts/Index.jsx
+++ frontend/src/Pages/Posts/Index.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 export function Index() {
     const [posts, setPosts] = useState([])
@@ -9,20 +10,32 @@ export function Index() {
             .then(res => {
                 setPosts(res.data)
             })
-            .catch(err => {
-                console.log("Error fetching data: ", err)
-            })
-    }, [])
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
 
     return (
         <div>
-            {posts && posts.map(post => (
-                <div key={post.id} className="p-5 my-5 border rounded-md shadow-sm text-left">
-                    <h2 className="mb-5 font-bold">{post.title}</h2>
-                    <p className="font-serif mb-3">{post.author}</p>
-                    <p>{post.body}</p>
-                </div>
-            ))}
+            <div className="flex">
+                <Link
+                    to="/posts/create"
+                    className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md hover:bg-blue-600 cursor-pointer"
+                >
+                    Create new post
+                </Link>
+            </div>
+
+            {posts && posts
+                .slice()
+                .reverse()
+                .map(post => (
+                    <div key={post.id} className="p-5 my-5 border rounded-md shadow-sm text-left">
+                        <h2 className="mb-5 font-bold">{post.title}</h2>
+                        <p className="font-serif mb-3">{post.author}</p>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
         </div>
     )
 }
